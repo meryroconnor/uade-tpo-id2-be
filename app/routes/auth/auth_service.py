00fb19e0b064d495d4606910a2e25a8718ba71ef
meryroconnor <maria.rivas@tiendanube.com>
2024-06-24 19:43:10 -0300

    sign up fix

--- app/routes/auth/auth_service.py
+++ app/routes/auth/auth_service.py
@@ -1,16 +1,23 @@
 from app.common.auth.auth_handler import sign_jwt
 from app.common.db_connectors.mysql import MysqlConn
 from app.dto.auth import SignUpDto, LoginDto
+from app.common.db_connectors.neo4j_conn import Neo4jConnector
 import bcrypt
 
 conn = MysqlConn()
-
+    
+    
 def signupService(req: SignUpDto):
     try:
         query = "insert into users(firstname, lastname, email, password, roles_id, nickname) values (%s, %s, %s, %s, %s, %s)"
         hashedPass = bcrypt.hashpw(req.password.encode('ASCII'), bcrypt.gensalt())
         data = (req.firstname, req.lastname, req.email, hashedPass, req.roles_id, req.nickname)
         rowId = conn.insert(query, data)
+        
+        connector = Neo4jConnector()
+        propierties = {'user_id':req.email,'user_name': req.firstname+req.lastname,'description': '','availability': 0,'image':'https://cdn-icons-png.flaticon.com/512/219/219969.png'}
+        connector.create_node('User', propierties)
+        
         return rowId
     except Exception as error:
         raise Exception(error)
